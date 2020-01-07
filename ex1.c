19f8f97102a460e3a0fd6c0c466ff35e125744c0
yristvi <56293232+yristvi@users.noreply.github.com>
2020-01-07 16:30:05 +0800

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+int c,gap,i,p,q;
+    for ( gap > 0 ; gap == 1 ; p = c + i ) ;
+    for(p > 0 ; q = p * p ; q > 0 )
+        while((q=getchar())&& q =='\n')
+    printf("%.2f\n",q);
+}
